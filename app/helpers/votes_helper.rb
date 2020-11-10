0ec386565e848a723b9677856a03821f2ace4bf9
rumbie <rumbiemudzie@gmail.com>
2020-11-10 20:50:32 +0200

    Create votes helper methods

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,11 @@
 module VotesHelper
+    def vote_or_Unvote(article)
+        vote = Vote.find_by(user: current_user, article: article)
+        
+        if vote
+        link_to 'Vote', article_vote_path(id: vote.id, article_id: article.id), method: :delete
+        else
+        link_to 'UnVote', article_votes_path(article.id), remote: true, method: :post
+        end
+        end 
 end
