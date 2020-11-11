bdfdac9dfbcc1dcdbb982070dacdd3f3a6f3f78a
rumbie <rumbiemudzie@gmail.com>
2020-11-11 22:39:23 +0200

    Modify all form styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
     if @user
       session[:user_id] = @user.id
-      redirect_to @user, notice: 'You are successfully logged in!'
+      redirect_to root_path, notice: 'You are successfully logged in!'
     else
       flash.now[:alert] = 'Invalid name'
       render :new
