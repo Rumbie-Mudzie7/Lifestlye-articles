a2fbc5b90b737af3978299e8f167246eea3094df
rumbie <rumbiemudzie@gmail.com>
2020-11-12 23:25:47 +0200

    Create unit tests for the vote model

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Vote, type: :model do
+  subject {Vote.new}
+
+  describe 'validations' do
+  it { should belong_to(:user) }
+  it { should belong_to(:article) }
+  end
+  
+end
