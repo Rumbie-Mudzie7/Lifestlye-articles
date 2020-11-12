7b7b2539511a15e34ac4833a77cf5667d7c3e736
rumbie <rumbiemudzie@gmail.com>
2020-11-12 22:26:34 +0200

    Create validations and associations tests for user_spec.rb model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,17 @@
 require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    subject { User.new }
+
+    describe 'validations' do
+        it {should validate_presence_of(:name) }
+        it {should validate_length_of(:name).is_at_most(20) }
+        it {should validate_uniqueness_of(:name).case_insensitive }
+    end
+
+    context 'associations', type: :model do
+       it {should have_many(:articles).class_name('Article') } 
+       it {should have_many(:votes)}
+    end
+end
+
