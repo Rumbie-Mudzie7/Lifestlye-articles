bfd8cc0c2d1e3429e9c1957ee5176685fcdad163
rumbie <rumbiemudzie@gmail.com>
2020-11-16 14:27:52 +0200

    Include edit user option on navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'You have successfully updated your profile' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
