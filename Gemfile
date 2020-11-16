85a899ddd9906127336f012f50b7ecef38252ad7
rumbie <rumbiemudzie@gmail.com>
2020-11-16 16:13:25 +0200

    Fix remaining errors

--- Gemfile
+++ Gemfile
@@ -41,7 +41,6 @@ group :development, :test do
   gem 'rspec-rails', '~> 4.0', '>= 4.0.1'
 
   gem 'database_cleaner', '~> 1.8', '>= 1.8.5'
-
 end
 
 group :development do
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,17 +1,17 @@
 module CategoriesHelper
-    def last_article_in_category(category)
-      category.articles.last if category.articles.any?
-    end
+  def last_article_in_category(category)
+    category.articles.last if category.articles.any?
+  end
 
-    def last_article_title(category)
-      if last_article_in_category(category)
-        last_article_in_category(category).title
-      else
-        'There are no articles under this category'
-      end
+  def last_article_title(category)
+    if last_article_in_category(category)
+      last_article_in_category(category).title
+    else
+      'There are no articles under this category'
     end
+  end
 
-    def category_article_image(category)
-        last_article_in_category(category)&.image if last_article_in_category(category)
-    end
+  def category_article_image(category)
+    last_article_in_category(category)&.image if last_article_in_category(category)
+  end
 end
