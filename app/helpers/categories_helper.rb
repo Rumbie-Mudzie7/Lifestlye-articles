bc1df32c05a1dce6ce1f38b66c8dab4839d27f84
rumbie <rumbiemudzie@gmail.com>
2020-11-16 13:35:29 +0200

    Refactor code in categories_helper.rb method

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -4,10 +4,14 @@ module CategoriesHelper
     end
 
     def last_article_title(category)
-      last_article_in_category(category).title
+      if last_article_in_category(category)
+        last_article_in_category(category).title
+      else
+        'There are no articles under this category'
+      end
     end
 
     def category_article_image(category)
-        last_article_in_category(category).image
+        last_article_in_category(category)&.image if last_article_in_category(category)
     end
 end
